24f10d094eabbba2d256a694f9c8c51f85b836aa
Gary Woodfine <gary.woodfine@threenine.co.uk>
2016-07-11 17:50:37 +0100

    Initial Commit

---
+++ functions/functions.php
@@ -0,0 +1,98 @@
+<?php
+function swc_plugin_activated()
+{
+	global $wp_swc_blacklist;
+
+	require_once (SWCPATH . "functions/crawlers.php");
+
+	swc_create_db();
+	swc_populate_db($wp_swc_blacklist);
+}
+
+
+function swc_create_db()
+{
+	global $wpdb;
+	require_once (ABSPATH . 'wp-admin/includes/upgrade.php');
+	// creates my_table in database if not exists
+	$table = $wpdb->prefix . "swc_blacklist";
+	$charset_collate = $wpdb->get_charset_collate();
+	$sql = "CREATE TABLE IF NOT EXISTS $table (
+	`id` mediumint(9) NOT NULL AUTO_INCREMENT,
+	`botnickname` varchar(30) NOT NULL,
+	`botname` text NOT NULL,
+	`boturl` text NOT NULL,
+	`botip` varchar(100) NOT NULL,
+	`botobs` text NOT NULL,
+	`botstate` varchar(10) NOT NULL,
+
+	UNIQUE (`id`),
+	UNIQUE (`botnickname`)
+
+	) $charset_collate;";
+
+	
+
+	dbDelta($sql);
+
+}
+
+
+function swc_plugin_db_update()
+{
+
+	global $wp_swc_blacklist, $wpdb;
+	require_once (ABSPATH . 'wp-admin/includes/upgrade.php');
+
+	require_once (SWCPATH . "functions/crawlers.php");
+
+	$z = count($wp_swc_blacklist);
+
+	$table_name = $wpdb->prefix . "swc_blacklist";
+
+	$results9 = $wpdb->get_results("SELECT * FROM $table_name");
+
+	if (count($results9) >= $z)
+		return;
+
+		swc_create_db();
+		swc_populate_db($wp_swc_blacklist);
+		
+}
+
+function swc_populate_db($wp_swc_blacklist)
+{
+	global $wpdb;
+	$table_name = $wpdb->prefix . "swc_blacklist";
+	$charset_collate = $wpdb->get_charset_collate();
+	$z = count($wp_swc_blacklist);
+
+
+	for ($i = 0; $i < $z; $i++) {
+		$a = $wp_swc_blacklist[$i];
+
+
+		$botnickname = trim($a['botnickname']);
+		$botname = trim($a['botname']);
+		$boturl = trim($a['boturl']);
+
+		$results9 = $wpdb->get_results("SELECT * FROM $table_name where botnickname = '$botnickname' limit 1");
+
+
+		if (count($results9) > 0 or empty($botnickname))
+			continue;
+
+
+			$query = "INSERT INTO ".$table_name.
+			" (botnickname, botname, boturl, botstate)
+          VALUES ('"
+					.$botnickname.
+					"', '".
+					$botname .
+					"', '"
+							.$boturl .
+							"', 'Enabled')";
+							 
+							$r = $wpdb->get_results($query);
+	}
+	}
---
+++ stop-web-crawlers.php
@@ -0,0 +1,102 @@
+<?php
+/*
+ Plugin Name: Stop Web Crawlers
+ Plugin URI: http://threenine.co.uk/product/stop-web-crawlers/
+ Description: Blocks traffic referrer spam bots
+ Version: 1.0.0
+ Author: Three Nine Consulting
+ Author URI: http://threenine.co.uk
+ License: GPLv2 or later
+ */
+
+
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+define('SWC', '0.2' );
+define('SWCPATH', plugin_dir_path(__file__) );
+define('SWCURL', plugin_dir_url(__file__));
+define('SWCDOMAIN', get_site_url() );
+
+
+// Add settings link on plugin page
+function swc_plugin_settings_link($links) {
+	$settings_link = '<a href="options-general.php?page=Stop-Web-Crawlers">Settings</a>';
+	array_unshift($links, $settings_link);
+	return $links;
+}
+
+$plugin = plugin_basename(__FILE__);
+add_filter("plugin_action_links_$plugin", 'swc_plugin_settings_link' );
+
+function swc_start() {
+	$wp_bs_loaded = new Stop_Web_Crawlers ();
+}
+
+require_once (SWCPATH . "settings/load-plugin.php");
+require_once (SWCPATH . "settings/options/plugin_options_tabbed.php");
+require_once (SWCPATH . "functions/functions.php");
+require_once(ABSPATH . 'wp-includes/pluggable.php');
+
+if ( ! class_exists( 'WP_List_Table' ) ) {
+	require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
+}
+
+require dirname( __FILE__ ) . '/includes/list-tables/class-swc-list-table.php';
+
+add_action( 'admin_menu', 'swc_add_menu_items' );
+
+function swc_add_menu_items() {
+	add_submenu_page(
+			'stop-web-crawlers', // $parent_slug
+			'Web Crawlers Table', // string $page_title
+			'Web Crawlers Table', // string $menu_title
+			'manage_options', // string $capability
+			'web-crawlers-table',
+			'swc_render_list_page' );
+}
+
+
+add_action( 'plugins_loaded', 'swc_start' );
+
+class Stop_Web_Crawlers {
+
+	public function __construct() {
+		add_action( 'parse_request', array( $this, 'swc_execute' ) );
+		
+	}
+
+	public function swc_execute() {
+		
+		global $wpdb;
+		$referer = isset( $_SERVER['HTTP_REFERER'] ) ? $_SERVER['HTTP_REFERER'] : false;
+
+		if ( empty( $referer ) ) {
+			return;
+		}
+
+		$referer = strtolower($referer);
+		$table_name = $wpdb->prefix . "swc_blacklist";
+		
+		$sql = "SELECT boturl FROM " . $table_name . " WHERE botstate = 'Enabled'";
+		$bots = $wpdb->get_results($sql) or die(mysql_error());
+
+		foreach($bots as $row) {
+			if ( strpos( $referer, $row->boturl ) !== false ) {
+				wp_die( '', '', array( 'response' => 403 ) );
+				exit;
+			}
+		}
+	}
+	
+	
+}
+	function swc_render_list_page() {
+		$test_list_table = new swc_List_Table();
+		$test_list_table->prepare_items();
+		require dirname( __FILE__ ) . '/includes/list-tables/page.php';
+	}
+	register_activation_hook( __FILE__, 'swc_plugin_activated');
+	add_action( 'plugins_loaded', 'swc_plugin_db_update' );
+
+?>
