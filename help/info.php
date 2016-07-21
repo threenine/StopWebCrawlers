4ac98ca0b2c54904e09679fb9c1d72fc7fb4694a
threenine <threenine@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-07-21 10:25:40 +0000

    Merge branch 'master' into svnsync
    git-svn-id: https://plugins.svn.wordpress.org/stop-web-crawlers/trunk@1458113 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- help/info.php
+++ help/info.php
@@ -8,32 +8,12 @@
 $ah_help = '
 <p style="box-sizing: inherit; border: 0px; font-family: Arial, Helvetica, sans-serif; font-size: 16px; font-style: normal; font-weight: normal; margin: 0px 0px 1.6842em; outline: 0px; padding: 0px; vertical-align: baseline; color: rgb(71, 71, 71); font-variant: normal; letter-spacing: normal; line-height: 31.9998px; orphans: auto; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px; ">
 1)&nbsp;
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-Open the page  <b><i>Stop Web Crawlers Table</i></b>&nbsp; (under Stop Web Crawlers Menu) and take a look at 
-Default  List.
-<span id="result_box" class="short_text" lang="en"><span class>If you wish,</span>
-<span class>you can turn off</span> <span class>some. (Just check, Bulk Actions, 
-Apply). Our plugin will create a table with 1113&nbsp; Bad Bots.</span></span></p>
-
-<p style="box-sizing: inherit; border: 0px; font-family: Arial, Helvetica, sans-serif; font-size: 16px; font-style: normal; font-weight: normal; margin: 0px 0px 1.6842em; outline: 0px; padding: 0px; vertical-align: baseline; color: rgb(71, 71, 71); font-variant: normal; letter-spacing: normal; line-height: 31.9998px; orphans: auto; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px; ">
-<span lang="en-us">2) At eMail Settings tab, you can customize your contact email or left 
-blank to use your wordpress eMail.</span></p>
-=======
->>>>>>> svnsync
 Open the page  <b><i>Stop Web Crawlers Table</i></b>&nbsp; 
 <span id="result_box" class="short_text" lang="en"><span class>If you wish,</span>
 <span class>you can turn off</span> <span class>some. (Just check, Bulk Actions, 
 Apply). Our plugin will create a table with 100&nbsp; of the most common referer spammer bots.</span></span></p>
 
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 94079315f100aed1bcffcacb31515052ac704fef
->>>>>>> svnsync
 <p style="box-sizing: inherit; border: 0px; font-family: Arial, Helvetica, sans-serif; font-size: 16px; font-style: normal; font-weight: normal; margin: 0px 0px 1.6842em; outline: 0px; padding: 0px; vertical-align: baseline; color: rgb(71, 71, 71); font-variant: normal; letter-spacing: normal; line-height: 31.9998px; orphans: auto; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; widows: 1; word-spacing: 0px; -webkit-text-stroke-width: 0px; ">
 Open the Plugin  <b><i>General Settings</i></b> Tab and click over Yes&nbsp; (to 
 begin to block).</p>
