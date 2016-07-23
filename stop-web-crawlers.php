032ba1900a917d1fdaa1be1822ddf0998d622a73
threenine <threenine@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-07-23 08:09:20 +0000

    Updated to version 1.0.6
    git-svn-id: https://plugins.svn.wordpress.org/stop-web-crawlers/trunk@1459419 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- stop-web-crawlers.php
+++ stop-web-crawlers.php
@@ -3,7 +3,7 @@
  Plugin Name: Stop Web Crawlers
  Plugin URI: http://threenine.co.uk/product/stop-web-crawlers/
  Description: Blocks traffic referrer spam bots
- Version: 1.0.5
+ Version: 1.0.6
  Author: Three Nine Consulting
  Author URI: http://threenine.co.uk
  License: GPLv2 or later
