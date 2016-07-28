b492b51c83f1f19f1401e439e462ed7291b9e33d
Gary Woodfine <gary.woodfine@threenine.co.uk>
2016-07-28 12:22:24 +0100

    Updated Dashbaord text

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,5 +1,19 @@
 
 <div class="wrap">
 <h2>Stop Web Crawler : Dashboard</h2>
+<p>There is zero configuration required for Stop Web Crawlers to start working.  Once you have activated the plugin Stop Web Crawlers starts block over 1200 known
+referer spammers web spiders from attacking your website.</p>
+<p>We do this by checking the HTTP object Referer property against the details of all known referer spammers we have listed in our database. If the referer matches they are redirected to 
+to a 403 permission denied page.</p>
 
+<p>To test Stop Web Crawlers is working on your site. You can use a browser tools like <a href="https://www.getpostman.com/" target="_blank">Postman</a> or our personal favourite
+HttpRequester.  For a detailed explanation on <a href="http://threenine.co.uk/test-stop-web-crawlers/" target="_blank"> how to test Stop Web Crawlers on your website</a></p>
+
+<p><strong>Please take note: </strong>  At present Stop Web Crawlers does not protect against what is known Ghost Referals.  Due in part because this type of referer spam does not 
+target your website directly, but actually targets your Google Analytics account directly. Find out how to <a href="http://threenine.co.uk/stop-referer-spam-google-analytics/ " target="_blank" > stop referer spam in Google Analytics</a> </p>
+
+<p>It is in our current product roadmap to automatically do this direct from the Stop Web Crawlers plugin. If you would like to vote for this functionality please visit our website
+and send us an email via the <a href="http://threenine.co.uk/contact/" target="_blank">Contact Page</a>  Or alternatively <a href="https://github.com/threenine/StopWebCrawlers/issues" target="_blank">raise an Issue on our Github</a>
+or the <a href="https://wordpress.org/support/plugin/stop-web-crawlers" target="_blank"></a> WordPress.org Stop Web Crawlers support tab</a>
+ </p>
 </div>
