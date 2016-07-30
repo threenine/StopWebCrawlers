eba79d72c6ea870cde17c49042f6b0baedaf4b40
threenine <threenine@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-07-30 19:53:31 +0000

    Updated Dashboard
    git-svn-id: https://plugins.svn.wordpress.org/stop-web-crawlers/trunk@1464015 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,6 +1,11 @@
 
 <div class="wrap">
 <h2>Stop Web Crawler : Dashboard</h2>
+<table width="100%">
+<tbody>
+<tr>
+<td width="70%">
+
 <p>There is zero configuration required for Stop Web Crawlers to start working.  Once you have activated the plugin Stop Web Crawlers starts block over 1200 known
 referer spammers web spiders from attacking your website.</p>
 <p>We do this by checking the HTTP object Referer property against the details of all known referer spammers we have listed in our database. If the referer matches they are redirected to 
@@ -10,10 +15,18 @@ to a 403 permission denied page.</p>
 HttpRequester.  For a detailed explanation on <a href="http://threenine.co.uk/test-stop-web-crawlers/" target="_blank"> how to test Stop Web Crawlers on your website</a></p>
 
 <p><strong>Please take note: </strong>  At present Stop Web Crawlers does not protect against what is known Ghost Referals.  Due in part because this type of referer spam does not 
-target your website directly, but actually targets your Google Analytics account directly. Find out how to <a href="http://threenine.co.uk/stop-referer-spam-google-analytics/ " target="_blank" > stop referer spam in Google Analytics</a> </p>
+target your website directly, but actually targets your Google Analytics account directly. Find out how to <a href="http://threenine.co.uk/how-to-stop-referer-spam-in-google-analytics/" target="_blank" > stop referer spam in Google Analytics</a> </p>
 
 <p>It is in our current product roadmap to automatically do this direct from the Stop Web Crawlers plugin. If you would like to vote for this functionality please visit our website
 and send us an email via the <a href="http://threenine.co.uk/contact/" target="_blank">Contact Page</a>  Or alternatively <a href="https://github.com/threenine/StopWebCrawlers/issues" target="_blank">raise an Issue on our Github</a>
 or the <a href="https://wordpress.org/support/plugin/stop-web-crawlers" target="_blank"></a> WordPress.org Stop Web Crawlers support tab</a>
  </p>
+ </td>
+ <td width="30%">
+ <a href="http://threenine.co.uk/wordpress-maintenance/" target="_blank"><img src="<?php echo plugins_url ( 'images/WordPressMaintenance.png',dirname(__FILE__));?>"/></a>
+ 
+ </td>
+ </tr>
+ </tbody>
+ </table>
 </div>
