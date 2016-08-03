259b473251ad93a235f0f67a0659e8036f76182c
threenine <threenine@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-08-03 16:02:12 +0000

    fucked merge
    git-svn-id: https://plugins.svn.wordpress.org/stop-web-crawlers/trunk@1467011 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions/functions.php
+++ functions/functions.php
@@ -1,22 +1,22 @@
 <?php
-function swc_plugin_activated()
-{
+function swc_plugin_activated() {
+	global $wp_version;
+	if (version_compare ( $wp_version, '4.1', '<' )) {
+		wp_die ( 'This plugin requires WordPress version 4.1 or higher.' );
+	}
 	global $wp_swc_blacklist;
-
+	
 	require_once (SWCPATH . "functions/crawlers.php");
-
-	swc_create_db();
-	swc_populate_db($wp_swc_blacklist);
+	
+	swc_create_db ();
+	swc_populate_db ( $wp_swc_blacklist );
 }
-
-
-function swc_create_db()
-{
+function swc_create_db() {
 	global $wpdb;
 	require_once (ABSPATH . 'wp-admin/includes/upgrade.php');
 	// creates my_table in database if not exists
 	$table = $wpdb->prefix . "swc_blacklist";
-	$charset_collate = $wpdb->get_charset_collate();
+	$charset_collate = $wpdb->get_charset_collate ();
 	$sql = "CREATE TABLE IF NOT EXISTS $table (
 	`id` mediumint(9) NOT NULL AUTO_INCREMENT,
 	`botnickname` varchar(30) NOT NULL,
@@ -30,69 +30,48 @@ function swc_create_db()
 	UNIQUE (`botnickname`)
 
 	) $charset_collate;";
-
 	
-
-	dbDelta($sql);
-
+	dbDelta ( $sql );
 }
-
-
-function swc_plugin_db_update()
-{
-
+function swc_plugin_db_update() {
 	global $wp_swc_blacklist, $wpdb;
 	require_once (ABSPATH . 'wp-admin/includes/upgrade.php');
-
+	
 	require_once (SWCPATH . "functions/crawlers.php");
-
-	$z = count($wp_swc_blacklist);
-
+	
+	$z = count ( $wp_swc_blacklist );
+	
 	$table_name = $wpdb->prefix . "swc_blacklist";
-
-	$results9 = $wpdb->get_results("SELECT * FROM $table_name");
-
-	if (count($results9) >= $z)
+	
+	$results9 = $wpdb->get_results ( "SELECT * FROM $table_name" );
+	
+	if (count ( $results9 ) >= $z)
 		return;
-
-		swc_create_db();
-		swc_populate_db($wp_swc_blacklist);
-		
+	
+	swc_create_db ();
+	swc_populate_db ( $wp_swc_blacklist );
 }
-
-function swc_populate_db($wp_swc_blacklist)
-{
+function swc_populate_db($wp_swc_blacklist) {
 	global $wpdb;
 	$table_name = $wpdb->prefix . "swc_blacklist";
-	$charset_collate = $wpdb->get_charset_collate();
-	$z = count($wp_swc_blacklist);
-
-
-	for ($i = 0; $i < $z; $i++) {
-		$a = $wp_swc_blacklist[$i];
-
-
-		$botnickname = trim($a['botnickname']);
-		$botname = trim($a['botname']);
-		$boturl = trim($a['boturl']);
-
-		$results9 = $wpdb->get_results("SELECT * FROM $table_name where botnickname = '$botnickname' limit 1");
-
-
-		if (count($results9) > 0 or empty($botnickname))
+	$charset_collate = $wpdb->get_charset_collate ();
+	$z = count ( $wp_swc_blacklist );
+	
+	for($i = 0; $i < $z; $i ++) {
+		$a = $wp_swc_blacklist [$i];
+		
+		$botnickname = trim ( $a ['botnickname'] );
+		$botname = trim ( $a ['botname'] );
+		$boturl = trim ( $a ['boturl'] );
+		
+		$results9 = $wpdb->get_results ( "SELECT * FROM $table_name where botnickname = '$botnickname' limit 1" );
+		
+		if (count ( $results9 ) > 0 or empty ( $botnickname ))
 			continue;
-
-
-			$query = "INSERT INTO ".$table_name.
-			" (botnickname, botname, boturl, botstate)
-          VALUES ('"
-					.$botnickname.
-					"', '".
-					$botname .
-					"', '"
-							.$boturl .
-							"', 'Enabled')";
-							 
-							$r = $wpdb->get_results($query);
+		
+		$query = "INSERT INTO " . $table_name . " (botnickname, botname, boturl, botstate)
+          VALUES ('" . $botnickname . "', '" . $botname . "', '" . $boturl . "', 'Enabled')";
+		
+		$r = $wpdb->get_results ( $query );
 	}
-	}
+}
