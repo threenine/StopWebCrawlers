71d0eea9cddf01f2587da74798cca67f27d957f4
Gary Woodfine <gary.woodfine@threenine.co.uk>
2016-09-14 10:27:40 +0100

    changing default schema

--- includes/swc-db-upgrade/dbscript/_1_3_5.php
+++ includes/swc-db-upgrade/dbscript/_1_3_5.php
@@ -2,21 +2,32 @@
 
 final class _1_3_5 extends updater{
 
-	private $SWC_CRAWLER_LOG = 'swc_crawler_log';
-	
+	private $SWC_CRAWLERS_LOG = 'swc_crawlers_log';
+	private $SWC_CRAWLERS = 'swc_crawlers';
 
 	public function __construct(){
 		require_once (ABSPATH . 'wp-admin/includes/upgrade.php');
 	}
 	
+	
 	public function update(){
 		
-		
+		$this->CreateCrawlerTable();
 		$this->CreateCrawlerLogTable();
 
-
-
 	}
+	
+	/*
+	 * Create Crawler table.
+	 *
+	 * Replacing initial SWC_Blacklist table .
+	 *
+	 * @since    1.3.5
+	 */
+	private function CreateCrawlerTable(){
+		
+	}
+	
 	/**
 	 * Create Crawler log table.
 	 *
@@ -28,14 +39,19 @@ final class _1_3_5 extends updater{
 
 		global $wpdb;
 		
-		$tableName = $wpdb->prefix . $this->SWC_CRAWLER_LOG;
+		$crawlerTable = $wpdb->prefix . $this->SWC_CRAWLERS;
+		$logTable= $wpdb->prefix . $this->SWC_CRAWLERS_LOG;
+		
         $charset_collate = $wpdb->get_charset_collate ();
 	
-		$sql = "CREATE TABLE IF NOT EXISTS $tableName (
-		`id` mediumint(9) NOT NULL AUTO_INCREMENT,
-		`botid` int NOT NULL,
-		`attempt` int NOT NULL,
-		UNIQUE (`id`)
+		$sql = "CREATE TABLE IF NOT EXISTS $logTable(
+		  `id` int(11) NOT NULL AUTO_INCREMENT,
+		  `crawlerid` mediumint(9) DEFAULT NULL,
+		  `attempts` mediumint(9) GENERATED ALWAYS AS (0) VIRTUAL,
+		  `lastAttempt` datetime DEFAULT NULL,
+		  PRIMARY KEY (`id`),
+		  KEY `id_idx` (`botid`),
+		  CONSTRAINT `id` FOREIGN KEY (`crawlerid`) REFERENCES `$crawlerTable' (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 		) $charset_collate;";
 		
 		
