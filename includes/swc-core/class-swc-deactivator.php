70c5d3780cecd92d8bb1a09a44b8fe3a583d1641
Gary Woodfine <gary.woodfine@threenine.co.uk>
2016-09-15 09:10:32 +0100

    Updated uninstall

---
+++ includes/swc-core/class-swc-deactivator.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.3.5
+ * @package    StopWebCrawlers
+ * @subpackage Plugin_Name/includes
+ * @author     threenine.co.uk <support@threenine.co.uk>
+ */
+class StopWebCrawlers_Deactivator {
+
+	/**
+	 * Uninstall plugin. 
+	 *
+	 * Drop all Plugin tables.
+	 *
+	 * @since    1.3.5
+	 */
+	public static function deactivate() {
+
+		// do any deactivation tasks.
+	}
+
+}
