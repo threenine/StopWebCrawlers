5929acf5dba2edc1e851b8eb5c35722934c62596
Gary Woodfine <gary.woodfine@threenine.co.uk>
2016-09-19 09:28:45 +0100

    auto update initial

--- includes/swc-core/class-swc-activator.php
+++ includes/swc-core/class-swc-activator.php
@@ -28,7 +28,21 @@ class StopWebCrawlers_Activator {
 		$dbcreate->create();		
 	}
 	
-	
+	private static function SetOptions(){
+		//Assign initial options
+		$activate_options = array
+		(
+				'subdomains'=> 'on',
+				'redirecturl'=> 'http://semalt.com',
+				'domains'=> '',
+				'update_url'=> 'https://api.github.com/repos/threenine/stopwebcrawlers/contents/list/referer.csv',
+				'last_update_time'=> '',
+				'blocked_count'=> 0,
+				'send_stats' => 'on'
+		);
+		
+		add_option( 'bot_block', $activate_options );
+	}
 	
 	
 	
